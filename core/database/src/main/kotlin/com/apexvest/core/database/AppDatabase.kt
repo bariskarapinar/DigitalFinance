47a21bb3f60ed284152b4b128522ea8bc1cf38aa
bariskarapinar <bkarapinar1@hotmail.com>
2026-06-10 15:39:39 +0300

    AppDatabase.kt Updated

--- core/database/src/main/kotlin/com/apexvest/core/database/AppDatabase.kt
+++ core/database/src/main/kotlin/com/apexvest/core/database/AppDatabase.kt
@@ -3,9 +3,12 @@ package com.apexvest.core.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.apexvest.core.database.dao.UserDao
+import com.apexvest.core.database.dao.WalletDao
 import com.apexvest.core.database.entity.UserEntity
+import com.apexvest.core.database.entity.WalletEntity
 
-@Database(entities = [UserEntity::class], version = 1, exportSchema = false)
+@Database(entities = [UserEntity::class, WalletEntity::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
+    abstract fun walletDao(): WalletDao
 }
